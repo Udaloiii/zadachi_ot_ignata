19282a34501e46bf0ce189a599326ca4c68d8b4a
Udaloiii <udaloiiisohappy@gmail.com>
2023-03-17 20:16:42 +0300

    Completed lesson 6

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -41,7 +41,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR}
                         onClick={handleClose}
                         // className={...} // делает студент
-                        className={(isActive) => isActive ? s.nav : ""}
+                        className={(isActive) => isActive ? s.active : ""}
                     >
                         Junior
                     </NavLink>
@@ -50,7 +50,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
                         // className={...} // делает студент
-                        className={(isActive) => isActive ? s.nav : ""}
+                        className={(isActive) => isActive ? s.active : ""}
                     >
                         Junior Plus
                     </NavLink>
