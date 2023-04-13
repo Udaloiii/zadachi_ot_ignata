9572fb0865ebf7027d168c9deac6a2d8d4ad950e
Udaloiii <udaloiiisohappy@gmail.com>
2023-04-13 20:37:51 +0300

    Completed 10 lesson

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
+import {InitialStateType, loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
@@ -15,12 +15,14 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const dispatch = useDispatch()
+    const isLoading = useSelector<AppStoreType>((state=> state.loading.isLoading))
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+        dispatch(loadingAC(true))
         // setTimeout
+        setTimeout(()=>dispatch(loadingAC(false)), 1500)
     }
 
     return (
@@ -41,6 +43,7 @@ const HW10 = () => {
                     </SuperButton>
                 )}
             </div>
+            <hr/>
         </div>
     )
 }
