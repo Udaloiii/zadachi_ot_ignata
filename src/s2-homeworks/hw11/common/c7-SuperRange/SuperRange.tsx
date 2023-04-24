1b28b7473293d20b8311cc605706d55f011a7406
Udaloiii <udaloiiisohappy@gmail.com>
2023-04-24 16:54:10 +0300

    Fixed bugs css in lesson 11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -8,7 +8,24 @@ const SuperRange: React.FC<SliderPropsType> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                width:"254px"
+                // width:"254px"
+                margin: "0 30px",
+                boxSizing: 'border-box',
+                width: '147px',
+                borderRadius: '10px',
+                '& .MuiSlider-rail': {
+                    color: '#8b8b8b',
+                },
+                '& .MuiSlider-thumb': {
+                    color: '#00CC22',
+                    border: '6px solid #00CC22',
+                },
+                '& .MuiSlider-thumb:before': {
+                    border: '5px solid white',
+                },
+                '& .MuiSlider-track': {
+                    color: '#00CC22',
+                }
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
